8fa9c27e91fe34b1b930d36b05b96879b5b2dd0c
unclespoony <drwspoon@gmail.com>
2022-12-11 16:20:43 -0500

    Update eks.tf

--- starter/infra/eks.tf
+++ starter/infra/eks.tf
@@ -19,7 +19,7 @@ module "project_eks" {
   account            = data.aws_caller_identity.current.account_id
   private_subnet_ids = module.vpc.private_subnet_ids
   vpc_id             = module.vpc.vpc_id
-  nodes_desired_size = 3
+  nodes_desired_size = 4
   nodes_max_size     = 10
   nodes_min_size     = 1
 
